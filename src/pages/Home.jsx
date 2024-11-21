8de25de496a3cdc993f12bc65a6a430d129de350
Supavate <supavate@gmail.com>
2024-11-21 17:17:12 +0700

    implement fade intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import Loader from "../components/Loader";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
-import { Suspense, useState, useCallback, useContext, useEffect } from "react";
+import { Suspense, useState, useCallback, useContext, useEffect, useRef } from "react";
 import Sky from "../models/Sky";
 import { OrbitControls } from "@react-three/drei";
 import IntroAnimation from "../camera/IntroAnimation";
@@ -10,8 +10,6 @@ import { BuildingContext } from "../context/buildingContext";
 import AllModels from "@components/AllModels";
 import DialogBox from "@components/DialogBox";
 
-
-
 export const Home = () => {
   document.title = "UTCC 3D Diagram"
   
@@ -19,7 +17,7 @@ export const Home = () => {
   const [introAnimateEnd, setIntroAnimateEnd] = useState(false);
   const handleIntroAnimate = (e) =>{
    setIntroAnimateEnd(e);
-  }
+  };
 
   //============== Camera positions and Orbit Controling Functions ===========
 
@@ -27,61 +25,54 @@ export const Home = () => {
 
 const {orbitPosition, setOrbitPosition} = useContext(BuildingContext); 
 const {cameraPosition, setCameraPosition} = useContext(BuildingContext);
-const {orbitAngle, setOrbitAngle} = useContext(BuildingContext)
-const {selectedModel} = useContext(BuildingContext)     
-  
-
-  
+const {orbitAngle, setOrbitAngle} = useContext(BuildingContext);
+const {selectedModel} = useContext(BuildingContext);
 
   return (
     <section className="w-full h-screen relative">
       <DialogBox/>
       
-       <Canvas className="w-full h-screen bg-transparent">
-          <Suspense fallback={<Loader/>}>
-               
-               <directionalLight/>
-               <ambientLight/>
-               <pointLight/>
-               <spotLight/>
-               <hemisphereLight/>
-               
-               <Sky/>
+      <Canvas className="w-full h-screen bg-transparent">
+        <Suspense fallback={<Loader/>}>
+          <Loader load={true}/>
 
-               {/* Orbit and Camera  */}
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+          
+          <Sky/>
+          
+          {/* Orbit and Camera  */}
+          {/* Component สำหรับการควบคุมมุมกล้อง (หมุนซ้าย-ขวา , หมุนขึ้น-ลง) */}
+          <OrbitControls
+             enableZoom={introAnimateEnd && selectedModel == null} 
+             enablePan={false} 
+             enableRotate={introAnimateEnd && selectedModel == null}
+             maxDistance={40} 
+             minDistance={selectedModel == null ? 15 : 2}
+             autoRotate={introAnimateEnd && selectedModel == null}
+             autoRotateSpeed={-0.25}
+             minPolarAngle={Math.PI / 5} //จำกัดมุมกล้องด้านบนสุด
+             maxPolarAngle={Math.PI / 2.25} //จำกัดมุมกล้องด้านล่างสุด
+             target={orbitPosition}
+          />
 
-               {/* Component สำหรับการควบคุมมุมกล้อง (หมุนซ้าย-ขวา , หมุนขึ้น-ลง) */}
-               <OrbitControls
-                  enableZoom={introAnimateEnd && selectedModel == null} 
-                  enablePan={false} 
-                  enableRotate={introAnimateEnd && selectedModel == null}
-                  maxDistance={40} 
-                  minDistance={selectedModel == null ? 15 : 2}
-                  autoRotate={introAnimateEnd && selectedModel == null}
-                  autoRotateSpeed={-0.25}
-                  minPolarAngle={Math.PI / 5} //จำกัดมุมกล้องด้านบนสุด
-                  maxPolarAngle={Math.PI / 2.25} //จำกัดมุมกล้องด้านล่างสุด
-                  target={orbitPosition}
-               />
+          {/* ควบคุมกล้องตอนโหลดหน้าใหม่ โดยเริ่มต้นจากระยะไกลๆ แล้วค่อยๆซูมเข้ามา */}
+          <IntroAnimation startAnimation={handleIntroAnimate} endAnimatePosition={cameraPosition}/>
 
-               {/* ควบคุมกล้องตอนโหลดหน้าใหม่ โดยเริ่มต้นจากระยะไกลๆ แล้วค่อยๆซูมเข้ามา */}
-               <IntroAnimation startAnimation={handleIntroAnimate} endAnimatePosition={cameraPosition}/>
+          {/* สำหรับการเปลี่ยนจุด Focus หรือ OrbitPosition */}
+          <CameraController
+             orbitPosition={orbitPosition} 
+             cameraPosition={cameraPosition} 
+             orbitAngle={orbitAngle}
+          /> 
 
-               {/* สำหรับการเปลี่ยนจุด Focus หรือ OrbitPosition */}
-               <CameraController
-                  orbitPosition={orbitPosition} 
-                  cameraPosition={cameraPosition} 
-                  orbitAngle={orbitAngle}
-               /> 
-
-               {/* All Models    */}
-               <AllModels/>
-               {/* <Loader/> */}
-               
-                
-            
-          </Suspense>
-       </Canvas>
+          {/* All Models    */}
+          <AllModels/>
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
